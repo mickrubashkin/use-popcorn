b831e7b8ad9edee6ead081ac185c251ac481b316
MickRubashkin <mikhail.rubashkin@gmail.com>
2023-08-31 08:05:11 +0200

    Add cleanup function to the effect (fetching movie list), to prevent race condition (multiple requests during the search)

--- src/App.js
+++ src/App.js
@@ -32,12 +32,15 @@ export default function App() {
 
   useEffect(
     function () {
+      const controller = new AbortController()
+
       async function fetchMovies() {
         try {
           setIsLoading(true)
           setError('')
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            { signal: controller.signal }
           )
 
           if (!res.ok) {
@@ -51,9 +54,12 @@ export default function App() {
           }
 
           setMovies(data.Search)
+          setError('')
           setIsLoading(false)
         } catch (error) {
-          setError(error.message)
+          if (error.name !== 'AbortError') {
+            setError(error.message)
+          }
         } finally {
           setIsLoading(false)
         }
@@ -66,6 +72,10 @@ export default function App() {
       }
 
       fetchMovies()
+
+      return function () {
+        controller.abort()
+      }
     },
     [query]
   )
